cf9db2e65efa26b6be0d7d7748ff68724b8b7412
sa <sa@gmail.com>
2021-01-30 09:28:11 +0000

    Initial commit

---
+++ test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+describe('Simple Math Test', () => {
+    it('Should retur 2', () => {
+        assert.strictEqual(1+1,2);
+    });
+    it('Should return 9', () => {
+        assert.strictEqual(3*3,9);
+    });
+});
